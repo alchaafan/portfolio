073dd437bcf5f89dfbc0ddb0505bc5a1e6a43b71
Muhammad <m.shaafan@outlook.de>
2025-06-18 10:36:07 +0200

    zweite section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
+import { MeComponent } from "./me/me.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, MeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
