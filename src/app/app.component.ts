a72903150bd5545ffed328ed7faf63cf1ee78f51
Muhammad <m.shaafan@outlook.de>
2025-06-19 08:39:16 +0200

    skills

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { MeComponent } from "./me/me.component";
 import { LetsworkComponent } from "./letswork/letswork.component";
+import { SkillsComponent } from "./skills/skills.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, MeComponent, LetsworkComponent],
+  imports: [RouterOutlet, HeaderComponent, MeComponent, LetsworkComponent, SkillsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
