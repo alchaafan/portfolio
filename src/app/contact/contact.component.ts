dc2c256f968f87afbf98894feeb580da24ad766c
Muhammad <m.shaafan@outlook.de>
2025-06-24 16:08:10 +0200

    contact logik

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,13 +1,48 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
 
-  
+  contactData = {
+    name: "",
+    email: "",
+    message: "",
+    privacy: false,
+  }
+
+   formErrors = {
+    name: '',
+    email: '',
+    message: '',
+    privacy: ''
+  }
+
+  validateForm() {
+    
+    this.formErrors = {
+      name: !this.contactData.name ? 'Please enter your name' : '',
+      email: !this.contactData.email ? 'Please enter your email' : '',
+      message: !this.contactData.message ? 'Please enter a message' : '',
+      privacy: !this.contactData.privacy ? 'Please accept the privacy policy' : ''
+    };
+    
+    
+    return Object.values(this.formErrors).every(error => error === '');
+  }
+
+
+  onSubmit(ngForm: NgForm) {
+    const isValid = this.validateForm();
+    if(ngForm.valid && ngForm.submitted) {
+       console.log(this.contactData)
+    }
+   
+  }
 
 }
--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -8,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class SkillsComponent {
 
+  skills = [
+    '/assets/images/Frame 6.png',
+    '/assets/images/Frame 7.png',
+    '/assets/images/Frame 8.png',
+    '/assets/images/Frame 9.png',
+    '/assets/images/Frame 10.png',
+    '/assets/images/Frame 11.png',
+    '/assets/images/Frame 12.png',
+    '/assets/images/Frame 13.png',
+    '/assets/images/Frame 18.png',
+    '/assets/images/Icon + Name s2.png'
+
+  ];
+
 }
