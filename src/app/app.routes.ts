812b520b52a6b162e351f4e05c24fb78b9a23ec1
Muhammad <m.shaafan@outlook.de>
2025-06-24 20:31:00 +0200

    impressum

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { MainpageComponent } from './mainpage/mainpage.component';
 import { PrivacyComponent } from './privacy/privacy.component';
+import { ImpressumComponent } from './impressum/impressum.component';
 
 
 
 export const routes: Routes = [
     {path: '', component: MainpageComponent},
+    {path: 'impressum', component: ImpressumComponent},
     {path: 'privacy', component: PrivacyComponent},
     {path: '**',redirectTo: ''}
 ];
