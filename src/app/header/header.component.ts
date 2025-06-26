745fa2c1ba144020cd618b8133961f7cb3945a06
Muhammad <m.shaafan@outlook.de>
2025-06-26 09:48:39 +0200

    responsive header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,14 +7,33 @@ import { TranslateModule, TranslateService } from '@ngx-translate/core';
   standalone: true,
   imports: [TranslateModule, CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-
-  
-
-constructor(private translate: TranslateService){}
+  constructor(private translate: TranslateService) {}
   changeLanguage(language: string) {
-     this.translate.use(language);
+    this.translate.use(language);
+  }
+  isMenuOpen = false;
+
+  closeMenu() {
+    const body = document.body;
+    this.isMenuOpen = false;
+    body.style.overflow = 'auto';
+  }
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+    const body = document.body;
+
+    if (this.isMenuOpen) {
+      body.style.overflow = 'hidden';
+    } else {
+      body.style.overflow = 'auto';
+    }
+  }
+
+  isMenuActive() {
+    return this.isMenuOpen ? 'open' : '';
   }
 }
--- src/app/mainpage/contact/contact.component.ts
+++ src/app/mainpage/contact/contact.component.ts
@@ -59,4 +59,6 @@ export class ContactComponent {
       console.log(this.contactData);
     }
   }
+
+  
 }
